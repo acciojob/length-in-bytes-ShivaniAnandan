12a392c0de3ae2a9393a94654c430ac0c4a9a9b9
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-09-14 19:44:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str]);
+
+ 
+  const sizeInBytes = blob.size;
+
+  return sizeInBytes;
 };
 
 // Do not change the code below
